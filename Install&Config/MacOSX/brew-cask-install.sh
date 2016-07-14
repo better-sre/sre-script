f37fb9acfbc70cddf87af8129e403fecd82c35ec
hhstore <hhstore@qq.com>
2016-07-14 17:32:17 +0800

    add - mac osx scripts.

---
+++ Install&Config/MacOSX/brew-cask-install.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+
+brew cask install atom
+
+# FTP 下载
+brew cask install filezilla
+
+# 虚拟机
+brew cask install virtualbox
+
+# JDK
+brew cask install java
+
+# 远程主机目录映射到本地
+brew cask install sshfs
+
+# B站
+brew cask install bilibili
+# 输入法
+brew cask install squirrel
+# 截图
+brew cask install snip
+
+# 管理 brew 安装的 service 的工具，
+# 安装之后可以看所有的 service 的运行状态
+brew search launchrocket
+
+
+
+
---
+++ Install&Config/MacOSX/brew-install.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+
+
+brew install git
+brew install mercurial
+brew install subversion
+
+brew install tmux
+brew install zsh
+brew install fish
+brew install mobile-shell
+
+brew install openssl
+
+brew install curl
+brew install wget
+
+brew install vim
+
+brew install autojump
+brew install tree
+
+brew install gcc
+brew install cmake
+brew install ctags
+brew install cscope
+
+brew install go
+brew install erlang
+
+brew install pyenv-virtualenv
+brew install pyenv
+
+# DB:
+brew install mongodb
+brew install mysql
+brew install redis
+brew install rabbitmq
+brew install sqlite
+
+
+brew install jenkins
+
+# 注意版本:
+brew install nginx
+brew install tomcat7
+
+brew install iperf3
