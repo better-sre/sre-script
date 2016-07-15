d93b320223a41160fa5819c05cd3e8606efc5a9d
hhstore <hhstore@qq.com>
2016-07-15 13:46:11 +0800

    add - 添加 centos zsh 安装脚本.

---
+++ Install&Config/CentOS/6.5-x64/ZSH/install-zsh.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+############################################################
+#
+# 参考: https://github.com/robbyrussell/oh-my-zsh/
+#
+############################################################
+
+
+sudo yum update -y
+
+# install
+sudo yum install -y zsh curl git
+
+
+
+# install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+#curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh
+
+
+
+# change default shell
+chsh -s /bin/zsh
+
+
+echo $SHELL
+
+
