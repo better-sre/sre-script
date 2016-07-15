9a5c471a5ba2fea0ffb1cda9c37acbe11ea4d2d4
hhstore <hhstore@qq.com>
2016-07-15 14:16:49 +0800

    update - 添加 tmux 安装方法, 调整目录结构.

---
+++ Install&Config/CentOS/6.5-x64/Git,SSH,Zsh,Tmux/install-tmux.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+sudo yum update -y
+
+sudo yum install -y tmux
+
+tmux -V
+# tmux 1.6
+
+
+
--- Install&Config/CentOS/6.5-x64/Git,SSH,Zsh,Tmux/install-zsh.sh
+++ Install&Config/CentOS/6.5-x64/Git,SSH,Zsh,Tmux/install-zsh.sh
@@ -12,18 +12,12 @@ sudo yum update -y
 # install
 sudo yum install -y zsh curl git
 
-
-
 # install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-
 #curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh
-
-
 
 # change default shell
 chsh -s /bin/zsh
-
 
 echo $SHELL
 
