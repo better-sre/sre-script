9d9b9df34c248d3ce1a19d0884db04c16374ac45
hhstore <hhstore@qq.com>
2016-07-19 16:36:43 +0800

    update - 更新安装方法.

--- CentOS/6.5,6.8-x64/Git,SSH,Zsh,Tmux/install-zsh.sh
+++ CentOS/6.5,6.8-x64/Git,SSH,Zsh,Tmux/install-zsh.sh
@@ -13,8 +13,16 @@ sudo yum update -y
 sudo yum install -y zsh curl git
 
 # install oh-my-zsh
+
+# 方法1:
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-#curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh
+
+# 方法2:
+# curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh
+
+# 方法3:
+# wget --no-check-certificate https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | sh
+
 
 # change default shell
 chsh -s /bin/zsh
