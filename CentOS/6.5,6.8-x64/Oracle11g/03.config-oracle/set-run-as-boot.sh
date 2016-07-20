f211180b48b6c60487d4cf8293b6b4c62a41f7b3
hhstore <hhstore@qq.com>
2016-07-20 15:33:08 +0800

    add - 设置开机启动 Oracle 脚本.

---
+++ CentOS/6.5,6.8-x64/Oracle11g/03.config-oracle/set-run-as-boot.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+#
+# 设置 Oracle 开机启动:
+#
+# vim /etc/oratab
+#
+# 修改最后一行: N -> Y
+#
+# orcl:/home/oracle/app/oracle/product/11.2.0/dbhome_1:N
+# orcl:/home/oracle/app/oracle/product/11.2.0/dbhome_1:Y
+#
+
