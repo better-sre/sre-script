0832be2ed812a88ae735f651d5ed334c2d84adc1
hhstore <hhstore@qq.com>
2016-07-25 10:51:22 +0800

    add - 添加 Mac 安装 pyenv, pyenv-virtualenv 脚本.

---
+++ MacOSX/Python/brew-install-pyenv.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+
+
+# 安装:
+brew install pyenv
+brew install pyenv-virtualenv
+
+
+
+# 升级:
+brew upgrade pyenv
+brew upgrade pyenv-virtualenv
+
+
+
+# 查看版本已过期软件:
+brew outdated
+
