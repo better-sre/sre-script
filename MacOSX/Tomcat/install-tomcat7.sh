a0c3b540e3cd8aa517d7f085896b89ae4aad96c1
hhstore <hhstore@qq.com>
2016-07-27 20:02:50 +0800

    add - Mac 安装 tomcat 日志

--- MacOSX/Tomcat/install-tomcat7.sh
+++ MacOSX/Tomcat/install-tomcat7.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
+# 安装 tomcat8:
+# 路径: /usr/local/Cellar/tomcat/8.5.4
+brew install tomcat
 
+
+# 安装 tomcat7:
 brew install tomcat7
-
-# tomcat 安装路径：
-# /usr/local/Cellar/tomcat/8.0.33/libexec/
 
