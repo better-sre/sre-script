2ff26d5f0ec97654a3419b7e8f1aab2dd1bfc3a0
hhstore <hhstore@qq.com>
2016-12-19 20:14:15 +0800

    update - mac npm packages.

--- MacOSX/Nodejs/cnpm-install.sh
+++ MacOSX/Nodejs/cnpm-install.sh
@@ -3,10 +3,22 @@
 cnpm install -g webpack
 cnpm install -g 
 
+# lib:
 cnpm install -g vue-cli
+cnpm install -g weex-toolkit
+cnpm install -g react-native-cli
+
+cnpm install -g marked
+
 
 # check es:
 cnpm install -g eslint
+
+# tool:
+cnpm install -g grunt-cli
+
+# check version:
+grunt --version
 
 
 
---
+++ MacOSX/fix-user-permission.sh
@@ -0,0 +1,7 @@
+
+# 如果你看到EACCES: permission denied这样的权限报错，
+# 那么请参照上文的homebrew译注，修复/usr/local目录的所有权：
+
+sudo chown -R `whoami` /usr/local
+
+
